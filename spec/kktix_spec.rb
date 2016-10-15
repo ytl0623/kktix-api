24103edb0f29257aa7c6ee9980a67a9e45d2ca47
JJC <jjc@nlplab.cc>
2016-10-15 15:36:04 +0800

    modify api spec

--- spec/kktix_spec.rb
+++ spec/kktix_spec.rb
@@ -2,24 +2,43 @@
 require 'minitest/autorun'
 require 'minitest/rg'
 require 'yaml'
-require_relative '../lib/kktix.rb'
+require_relative '../lib/organization.rb'
 
-test_org = 'nthuion'
+TEST_ORG_ID = 'nthuion'
+KKTIX_RESPONSE = YAML.load(File.read('spec/fixtures/kktix_response.yml'))
+TEST_ORG_INFO = YAML.load(File.read('spec/fixtures/nthuion_info.yml'))
 
-describe 'KKTIC spec' do
-  it 'should be able to get data throuh api' do
-    Activities.new.org_acts(test_org).wont_be_empty
+describe 'KKTIX API spec' do
+  before do
+    @kktix_api = KktixEvent::KktixApi.new
+    @organization = KktixEvent::Organization.new(@kktix_api, TEST_ORG_ID)
   end
 
-  it 'should get the title of the activity' do
-    Activities.new.org_acts(test_org)['title'].wont_be_empty
+  it 'should be able to get title throuh api' do
+    @kktix_api.title.length.must_be :>, 0
   end
 
-  it 'should get the updated time of the activity' do
-    Activities.new.org_acts(test_org)['updated'].wont_be_empty
+  it 'should be able to get last updated time throuh api' do
+    @kktix_api.updated.length.must_be :>, 0
   end
 
-  it 'should get the entry of the activity' do
-    Activities.new.org_acts(test_org)['entry'].wont_be_empty
+  it 'should be able to get events information throuh api' do
+    @kktix_api.events.wont_be_empty
+  end
+
+  it 'should be the same oid of the organization' do
+    @organization.oid.must_equal TEST_ORG_ID
+  end
+
+  it 'should be able to get name of the organization' do
+    @organization.name.length.must_be :>, 0
+  end
+
+  it 'should be able to get uri of the organization' do
+    @organization.uri.length.must_be :>, 0
+  end
+
+  it 'should be able to get events the organization' do
+    @organization.events.wont_be_empty
   end
 end
