fd3a94d0682599b496604dd1b240fd6c655a1009
JJC <jjc@nlplab.cc>
2016-10-21 15:21:00 +0800

    little adjustment

--- spec/kktix_spec.rb
+++ spec/kktix_spec.rb
@@ -1,11 +1,6 @@
 # frozen_string_literal: true
 require_relative 'spec_helper.rb'
 
-TEST_ORG_ID = 'nthuion'
-# KKTIX_RESPONSE = YAML.load(File.read('spec/fixtures/kktix_response.yml'))
-# TEST_ORG_INFO = YAML.load(File.read('spec/fixtures/nthuion_info.yml'))
-
-
 describe 'KKTIX API spec' do
   VCR.configure do |c|
     c.cassette_library_dir = CASSETTES_FOLDER
@@ -34,6 +29,24 @@ describe 'KKTIX API spec' do
   it 'should be able to get events information throuh api' do
     @kktix_api.events.wont_be_empty
   end
+end
+
+describe 'KKTIX API spec' do
+  VCR.configure do |c|
+    c.cassette_library_dir = CASSETTES_FOLDER
+    c.hook_into :webmock
+  end
+
+  before do
+    VCR.insert_cassette CASSETTE_FILE, record: :new_episodes
+
+    @kktix_api = KktixEvent::KktixApi.new
+    @organization = KktixEvent::Organization.new(@kktix_api, TEST_ORG_ID)
+  end
+
+  after do
+    VCR.eject_cassette
+  end
 
   it 'should be the same oid of the organization' do
     @organization.oid.must_equal TEST_ORG_ID
@@ -50,5 +63,4 @@ describe 'KKTIX API spec' do
   it 'should be able to get events the organization' do
     @organization.events.wont_be_empty
   end
-
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 require 'minitest/autorun'
 require 'minitest/rg'
 require 'yaml'
@@ -5,6 +6,10 @@ require 'vcr'
 require 'webmock'
 require_relative '../lib/organization.rb'
 
-FIXTURES_FOLDER = 'fixtures'
+FIXTURES_FOLDER = 'spec/fixtures'
 CASSETTES_FOLDER = "#{FIXTURES_FOLDER}/cassettes"
 CASSETTE_FILE = 'kktix_api'
+
+TEST_ORG_ID = 'nthuion'
+# KKTIX_RESPONSE = YAML.load(File.read('spec/fixtures/kktix_response.yml'))
+# TEST_ORG_INFO = YAML.load(File.read('spec/fixtures/nthuion_info.yml'))
