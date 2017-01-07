ad6e19636dc5049487aadf5987c30610f8b4dabc
JJC <jjc@nlplab.cc>
2017-01-07 17:52:13 +0800

    add search api

--- lib/kktix-api/kktix_api.rb
+++ lib/kktix-api/kktix_api.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 require 'yaml'
 require 'http'
+require 'open-uri'
+require 'nokogiri'
+require 'uri'
 
 module KktixEvent
   # Service for retriving KKTIX events
@@ -11,9 +14,43 @@ module KktixEvent
       []
     end
 
+    def self.search(query)
+      uri = URI.escape("https://kktix.com/events?search=#{query}")
+      doc = Nokogiri::HTML(open(uri))
+      parse_search_result(doc)
+    end
+
     def self.events_json_uri(oid: nil)
       return 'https://kktix.com/events.json' if oid.nil?
       "http://#{oid}.kktix.cc/events.json"
+    end
+
+    private_class_method
+
+    def self.parse_search_result(doc)
+      events = doc.css('ul.event-list > li').map do |node|
+        title, url = parse_title node
+        date = node.css('.date').text
+        author = parse_author(node)
+        summary = node.css('> .description').text
+
+        { url: url, title: title, date: date, summary: summary, author: author }
+      end
+      events
+    end
+
+    def self.parse_title(node)
+      title_node = node.css('> h2 > a')
+      title = title_node.text.strip
+      url = title_node.attr('href').value
+      [title, url]
+    end
+
+    def self.parse_author(node)
+      author_node = node.css('.host')
+      name = author_node.text.strip
+      uri = author_node.css('> a').attr('href').value
+      { name: name, uri: uri }
     end
   end
 end
